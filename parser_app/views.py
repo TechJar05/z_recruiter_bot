db0ce60fac99543c56f4d87e57c823eadd207d19
ddnyanu <dhotrednayanu@gmail.com>
2025-08-06 14:20:15 +0530

    gunicron added

--- parser_app/views.py
+++ parser_app/views.py
@@ -130,7 +130,7 @@ class RegenerateSummaryAPIView(APIView):
 
 
 
-# linkedin parsing
+# # linkedin parsing
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -167,3 +167,8 @@ class ApolloLinkedInLookupAPIView(APIView):
 
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
+
+
